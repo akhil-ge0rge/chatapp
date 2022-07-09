254e416a0da692145772a363c6696749345a49b4
Akhil George <akhil8311@gmail.com>
2022-07-09 12:04:57 +0530

    progress indicator and circle avatar added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -40,31 +40,47 @@ class _HomeScreenState extends State<HomeScreen> {
                 .where("userid", isNotEqualTo: userId)
                 .snapshots(),
             builder: (context, snapshot) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) {
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
+                return Text("No Chats ");
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
                             return ChatPage();
-                          },
-                        ));
-                      },
-                      child: Card(
-                        margin: EdgeInsets.all(10),
-                        child: Container(
-                          padding: EdgeInsets.only(left: 10),
-                          height: 70,
-                          child: Row(
-                            children: [
-                              Text(snapshot.data!.docs[index]['username']),
-                            ],
+                          }));
+                        },
+                        child: Card(
+                          margin: const EdgeInsets.all(10),
+                          child: Container(
+                            padding: const EdgeInsets.only(left: 10),
+                            height: 70,
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 25,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(50),
+                                    child: Image.network(snapshot
+                                        .data!.docs[index]['userimage']),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: scrWidth / 15,
+                                ),
+                                Text(snapshot.data!.docs[index]['username']),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  });
+                      );
+                    });
+              }
             }),
       ),
     );
